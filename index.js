6e466d30713f85b7ddd78add3eaeee786aba3e4c
Varun Mehta <varun.sagi20@gmail.com>
2018-02-06 23:55:28 +0530

    fixing crash caused by use of schema in passport before it is registered by userjs by ordering userjs above passports

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
-require('./services/passport');
 require('./models/user');
+require('./services/passport');
 
 mongoose.connect(keys.mongoDevUri);
 const app = express();
