a5f052d32f6a618b6d083600e68e6e53fd82f2ba
Varun Mehta <varun.sagi20@gmail.com>
2018-02-07 14:59:10 +0530

    configuring & enabling cookies in passport

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
 require('./models/user');
@@ -7,6 +9,18 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoDevUri);
 const app = express();
+
+// Configure cookies
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,               // 30 days in ms
+        keys: [keys.cookieKey, keys.cookieKey2]         // More than 1 key could be provided to provide additional layer of security
+    })
+)
+
+// Enable cookies in passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Register routes
 require('./routes/routeGoogleAuthentication')(app);
