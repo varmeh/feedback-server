b1ae8d6aaffaeaac3bc05df288201c2dc60ad904
Varun Mehta <varun.sagi20@gmail.com>
2018-02-07 12:58:03 +0530

    mongoose implementation for user instance creation. (duplication fixed)

--- services/passport.js
+++ services/passport.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const keys = require('../config/keys');
 
-const User = mongoose.model('users')
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -13,7 +13,18 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            new User({ googleId: profile.id }).save()
+            User.findOne({ googleId: profile.id })
+                .then(existingUser => {
+                    if (existingUser) {
+                        // user found
+                        done(null, existingUser)
+                    } else {
+                        // no user found & existing user === null
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user))
+                    }
+                })
         }
     )
 );
