fdd4f380e955f61f0dedd88d72689d0ce678186f
Varun Mehta <varun.sagi20@gmail.com>
2018-02-12 09:09:38 +0530

    prod env key file

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoUri: process.env.MONGO_URI,
+    cookieKey1: process.env.COOKIE_KEY_1,
+    cookieKey2: process.env.COOKIE_KEY_2
+}
