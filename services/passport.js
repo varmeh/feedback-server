9298bf1da6af4bff6e0c69c67eb758f6521ad8ef
Varun Mehta <varun.sagi20@gmail.com>
2018-02-13 18:11:37 +0530

    refactored: promise to async-await

--- services/passport.js
+++ services/passport.js
@@ -20,19 +20,15 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })
-                .then(existingUser => {
-                    if (existingUser) {
-                        // user found
-                        done(null, existingUser)
-                    } else {
-                        // no user found & existing user === null
-                        new User({ googleId: profile.id })
-                            .save()
-                            .then(user => done(null, user))
-                    }
-                })
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id })
+            if (existingUser) {
+                // user found
+                return done(null, existingUser)
+            }   
+            // no user found & existing user === null
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user)
         }
     )
 );
