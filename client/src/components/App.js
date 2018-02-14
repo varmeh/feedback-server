ade35040b83c8fcaa7d8fa865e320ac03cd8644f
Varun Mehta <varun.sagi20@gmail.com>
2018-02-14 18:20:45 +0530

    refactoring header from Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
@@ -8,13 +8,13 @@ const Landing = () => <h2>Landing</h2>
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <Router>
                 <div>
-                    <Route path="/" component={Header} />
+                    <Header />
                     <Route exact path="/" component={Landing} />
                     <Route path="/surveys" component={Dashboard} />
                 </div>
-            </BrowserRouter>
+            </Router>
         )
     }
 }
