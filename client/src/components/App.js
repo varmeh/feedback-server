2b137b707fc29d9198e7694aa6cf79b9ffc20610
Varun Mehta <varun.sagi20@gmail.com>
2018-02-14 18:15:25 +0530

    test Routing done with dummy components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Hello There!
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={Header} />
+                    <Route exact path="/" component={Landing} />
+                    <Route path="/surveys" component={Dashboard} />
+                </div>
+            </BrowserRouter>
         )
     }
 }
