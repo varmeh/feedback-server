669329662bf91bd89942ac20a9da740138246954
Varun Mehta <varun.sagi20@gmail.com>
2018-02-14 17:35:47 +0530

    rootReducer added to redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 
+import rootReducer from './reducers'
+
 const middlewareProd = []
 const middlewareDev = [ ...middlewareProd ]
 const middleware = process.env.NODE_ENV === 'production' ? middlewareProd : middlewareDev
 
-export default createStore(() => [], applyMiddleware(...middleware))
+export default createStore(rootReducer, applyMiddleware(...middleware))
