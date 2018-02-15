7cb25c60cb97fb737a91c18a6bc3c4299122808f
Varun Mehta <varun.sagi20@gmail.com>
2018-02-15 16:09:33 +0530

    adding header to appjs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-const Header = () => <h2>Header</h2>
+import Header from './Header'
 const Dashboard = () => <h2>Dashboard</h2>
 const Landing = () => <h2>Landing</h2>
 
@@ -9,7 +9,7 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <div>
+                <div className="container">
                     <Header />
                     <Route exact path="/" component={Landing} />
                     <Route path="/surveys" component={Dashboard} />
