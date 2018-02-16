4fc8268584dba4c7e20d755a5a20be46bae86d02
Varun Mehta <varun.sagi20@gmail.com>
2018-02-16 13:07:43 +0530

    client: auth action call from cdm

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import { fetchUser } from '../redux/actions'
 
 import Header from './Header'
 const Dashboard = () => <h2>Dashboard</h2>
 const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser()
+    }
+
     render() {
         return (
             <Router>
@@ -19,4 +26,4 @@ class App extends Component {
     }
 }
 
-export default App
+export default connect(null, { fetchUser })(App)
