3fd04fac757a0f182eae5c34caa808186c0823e3
Varun Mehta <varun.sagi20@gmail.com>
2018-02-16 11:21:24 +0530

    middleware thunk & logger applied to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
 import rootReducer from './reducers'
 
-const middlewareProd = []
-const middlewareDev = [ ...middlewareProd ]
+const middlewareProd = [ thunk ]
+const middlewareDev = [ ...middlewareProd, logger ]
 const middleware = process.env.NODE_ENV === 'production' ? middlewareProd : middlewareDev
 
 export default createStore(rootReducer, applyMiddleware(...middleware))
