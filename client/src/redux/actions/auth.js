f11685a1db8cab4fbdcc69e915a62f506a76f99d
Varun Mehta <varun.sagi20@gmail.com>
2018-02-16 13:10:59 +0530

    client: refactor: async implementation for fetchUser

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import { types } from '../actions'
 
-export const fetchUser = () => dispatch => {
-    axios.get('/api/current_user')
-        .then(res => dispatch({ type: types.FETCH_USER, payload: res }))
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user')
+    dispatch({ type: types.FETCH_USER, payload: res })
 }
