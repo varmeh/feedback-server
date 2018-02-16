9c96278f034ed6f44a63b566d38f5b33f65f9c83
Varun Mehta <varun.sagi20@gmail.com>
2018-02-16 15:47:46 +0530

    client: auth reducer implementation

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -4,5 +4,5 @@ import { types } from '../actions'
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user')
-    dispatch({ type: types.FETCH_USER, payload: res })
+    dispatch({ type: types.FETCH_USER, payload: res.data })
 }
--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,5 +1,15 @@
-export default (state = {}, action) => {
+import { types } from '../actions'
+
+const INITIAL_STATE = {
+    loggedIn: null,
+    user: null
+}
+
+export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case types.FETCH_USER:
+            return { ...state, loggedIn: action.payload !== '', user: action.payload }
+
         default:
             return state
     }
