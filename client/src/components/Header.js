669aff0f8ed468f7fe66b553edf534bd95250ddd
Varun Mehta <varun.sagi20@gmail.com>
2018-02-16 16:49:57 +0530

    client: adding link to brand logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
     renderContent() {
@@ -19,7 +20,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">Feedbck</a>
+                    <Link 
+                        className="left brand-logo" 
+                        to={this.props.loggedIn ? '/surveys' : '/'}
+                    >
+                        Feedbck
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
